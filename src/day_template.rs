d111d07ad9a7111c2b7a2a00336ddcea29e3ad33
Ben Cheatham <Benjamin.Cheatham@amd.com>
2024-08-07 11:17:44 -0500

    Add reading from input file to day_template
    All of the days have imported std::fs and used fs::read_to_string(<input
    file>), so add to day_template.rs.
    
    Signed-off-by: Ben Cheatham <Benjamin.Cheatham@amd.com>

--- src/day_template.rs
+++ src/day_template.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::fs;
 
 #[allow(unused)]
 fn part1(input: &str) {
@@ -11,5 +12,6 @@ fn part2(input: &str) {
 }
 
 pub fn run() -> io::Result<()> {
+    let input = fs::read_to_string("");
     Ok(())
 }

c5563270e12667a00d1805f662ab9038d202c3cd
Ben Cheatham <Benjamin.Cheatham@amd.com>
2024-08-08 14:26:39 -0500

    Add Directions enum to utils module
    Enum is intended to be used for array traversals in 4 directions (Up,
    Down, Left, Right). Implements To<u64> and To<i64>
    
    Signed-off-by: Ben Cheatham <Benjamin.Cheatham@amd.com>

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,1 +1,33 @@
 mod graph;
+
+#[derive(Debug, Clone, Copy)]
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl From<Direction> for u64 {
+    fn from(value: Direction) -> Self {
+        use Direction::*;
+
+        match value {
+            Up => 1,
+            Down => 2,
+            Left => 3,
+            Right => 4,
+        }
+    }
+}
+
+impl From<Direction> for i64 {
+    fn from(value: Direction) -> Self {
+        use Direction::*;
+
+        match value {
+            Up | Left => -1,
+            Down | Right => 1,
+        }
+    }
+}

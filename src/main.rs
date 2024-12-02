c68ae68bdcda8ba00cf473d7d34080349efaff53
Ben Cheatham <Benjamin.Cheatham@amd.com>
2024-12-02 09:08:39 -0600

    Update main.rs to allow just downloading inputs
    Also improve error handling and create missing input directories.
    
    Signed-off-by: Ben Cheatham <Benjamin.Cheatham@amd.com>

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use reqwest::Client;
 use std::fs::{self, File};
 use std::io::Write;
 use std::io::{self, Read};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 mod utils;
 #[path = "2022/mod.rs"]
@@ -37,12 +37,15 @@ enum Commands {
     Create {
         #[arg(short, long)]
         year: String,
-        #[arg(short, long)]
+        #[arg(short, long, default_value_t = 0)]
         day: usize,
         // Session key for getting input. If not specified, this command will only create a template
         // file
         #[arg(short, long)]
         session: Option<PathBuf>,
+        // Only get inputs for given year (day is optional)
+        #[arg(long)]
+        download: bool,
     },
 }
 
@@ -52,36 +55,75 @@ async fn main() -> io::Result<()> {
 
     match &args.command {
         Some(Commands::Run { year, day }) => run(year, *day),
-        Some(Commands::Create { year, day, session }) => create(year, *day, session.clone()).await,
+        Some(Commands::Create {
+            year,
+            day,
+            session,
+            download,
+        }) => create(year, *day, session.clone(), *download).await,
         _ => Ok(()),
     }
 }
 
-async fn create(year: &str, day: usize, session: Option<PathBuf>) -> io::Result<()> {
-    let mut template = File::open("src/day_template.rs")?;
-    let mut day_file = File::create(format!("src/{}/day{}.rs", year, day))?;
-    let mut buf = Vec::new();
+async fn create(
+    year: &str,
+    day: usize,
+    session: Option<PathBuf>,
+    download: bool,
+) -> io::Result<()> {
+    if !download {
+        let mut template = File::open("src/day_template.rs").expect("Could not find day template");
+        let mut day_file =
+            File::create(format!("src/{}/day{}.rs", year, day)).expect("Error creating day file");
+        let mut buf = Vec::new();
 
-    template.read_to_end(&mut buf)?;
-    day_file.write_all(&buf)?;
+        template.read_to_end(&mut buf)?;
+        day_file.write_all(&buf)?;
+    } else if session.is_none() {
+        return Err(io::Error::new(
+            io::ErrorKind::Other,
+            "no session cookie specified, required for downloading inputs!",
+        ));
+    }
 
     if session.is_none() {
         return Ok(());
     }
 
-    let key = fs::read_to_string(session.unwrap())?;
-    let mut input = File::create(format!("inputs/{}/day{}.txt", year, day))?;
-    let url = format!("https://adventofcode.com/{}/day/{}/input", year, day);
+    let key = fs::read_to_string(session.unwrap()).expect("Could not find session cookie file");
+    download_inputs(&key, year, day).await
+}
 
-    if let Some(text) = send_request(&key, &url).await {
-        input.write_all(text.as_bytes())?;
-        Ok(())
-    } else {
-        Err(io::Error::new(
-            io::ErrorKind::Other,
-            "Error getting puzzle input",
-        ))
+async fn download_inputs(key: &str, year: &str, day: usize) -> io::Result<()> {
+    let year_path = format!("inputs/{}", year);
+
+    if !Path::new(&year_path).exists() {
+        fs::create_dir(year_path)?;
     }
+
+    let (lower, upper) = if day == 0 { (1, 25) } else { (day, day) };
+
+    for i in lower..=upper {
+        let day_path = format!("inputs/{}/day{}.txt", year, i);
+
+        if Path::new(&day_path).exists() {
+            continue;
+        }
+
+        let mut input = File::create(day_path)?;
+        let url = format!("https://adventofcode.com/{}/day/{}/input", year, i);
+
+        if let Some(text) = send_request(key, &url).await {
+            input.write_all(text.as_bytes())?;
+        } else {
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                format!("Error getting puzzle input for {} day {}", year, i),
+            ));
+        }
+    }
+
+    Ok(())
 }
 
 async fn send_request(session: &str, url: &str) -> Option<String> {

69480b136ddebf113447037ffafeea237482d254
Ben Cheatham <benhartcheatham@gmail.com>
2024-12-07 11:09:39 -0600

    Optimize 2024 day 7
    Go from the back instead of the front of the operands, and apply the
    inverse operation to the running total until you get the goal. For
    example:
        192: 17 8 14 =>
    	192 - 14 =>
    	178 // 8 =>
    	17 - 17 (or any of them really) =>
    	0 == 0
    
    instead of:
        192: 17 8 14 =>
    	17 || 8  =>
    	178 + 14 =>
    	192 == 192
    
    Signed-off-by: Ben Cheatham <benhartcheatham@gmail.com>

--- src/2024/day7.rs
+++ src/2024/day7.rs
@@ -14,11 +14,29 @@ impl Ops {
         use Ops::*;
 
         match self {
-            Add => lhs + rhs,
-            Mul => lhs * rhs,
-            Concat => (lhs.to_string() + &rhs.to_string())
+            Add => lhs - rhs,
+            Mul => lhs / rhs,
+            Concat => lhs
+                .to_string()
+                .strip_suffix(&rhs.to_string())
+                .unwrap()
                 .parse::<usize>()
-                .unwrap(),
+                .unwrap_or(0),
+        }
+    }
+
+    fn is_applicable(&self, lhs: usize, rhs: usize) -> bool {
+        use Ops::*;
+
+        match self {
+            Add => lhs > rhs,
+            Mul => lhs % rhs == 0,
+            Concat => {
+                let lhs = lhs.to_string();
+                let rhs = rhs.to_string();
+
+                lhs != rhs && lhs.ends_with(&rhs)
+            }
         }
     }
 }
@@ -39,13 +57,21 @@ impl Equation {
     }
 
     fn solve_helper(&self, curr: usize, idx: usize) -> bool {
-        if idx == self.operands.len() {
-            return curr == self.goal;
+        if idx == 0 {
+            return self
+                .allowed_ops
+                .iter()
+                .filter(|op| op.is_applicable(curr, self.operands[idx]))
+                .any(|op| match op {
+                    Ops::Mul => op.apply(curr, self.operands[idx]) == 1,
+                    _ => op.apply(curr, self.operands[idx]) == 0,
+                });
         }
 
         self.allowed_ops
             .iter()
-            .map(|op| self.solve_helper(op.apply(curr, self.operands[idx]), idx + 1))
+            .filter(|op| op.is_applicable(curr, self.operands[idx]))
+            .map(|op| self.solve_helper(op.apply(curr, self.operands[idx]), idx - 1))
             .any(|e| e)
     }
 
@@ -55,7 +81,8 @@ impl Equation {
         } else if self.operands.len() == 1 {
             self.operands[0] == self.goal
         } else {
-            self.solve_helper(self.operands[0], 1)
+            let idx = self.operands.len() - 1;
+            self.solve_helper(self.goal, idx)
         }
     }
 }

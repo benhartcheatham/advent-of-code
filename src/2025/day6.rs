fa91f6c53a5b4308e25ed47f1cfb07087f6f8ddb
Ben Cheatham <benhartcheatham@gmail.com>
2025-12-06 12:34:07 -0600

    Clearer solution for 2025 day 6 part 2
    Signed-off-by: Ben Cheatham <benhartcheatham@gmail.com>

--- src/2025/day6.rs
+++ src/2025/day6.rs
@@ -23,7 +23,7 @@ fn part1(input: &str) -> usize {
     for line in lines.iter().take(lines.len() - 1) {
         for (i, n) in line
             .split_whitespace()
-            .map(|s| s.parse::<usize>().unwrap())
+            .filter_map(|s| s.parse::<usize>().ok())
             .enumerate()
         {
             answers[i] = match ops[i] {
@@ -38,54 +38,47 @@ fn part1(input: &str) -> usize {
 }
 
 fn part2(input: &str) -> usize {
-    let lines: Vec<&str> = input.lines().collect();
-    let last = lines.last().unwrap();
-    let ops: Vec<char> = last
-        .split_whitespace()
-        .map(|s| s.chars().next().unwrap())
-        .collect();
-    let mut lens: Vec<usize> = last.split(&['+', '*']).map(|s| s.len()).skip(1).collect();
-    let mut problems = Vec::new();
+    let lines: Vec<Vec<char>> = input.lines().map(|s| s.chars().collect()).collect();
+    let mut numbers = Vec::new();
+    let mut answers = Vec::new();
+    let mut skip = false;
 
-    *lens.last_mut().unwrap() += 1;
-    for len in lens.iter().take(ops.len()) {
-        problems.push(vec![String::new(); *len]);
-    }
+    for j in (0..(lines[0].len())).rev() {
+        if skip {
+            numbers.clear();
+            skip = false;
+            continue;
+        }
 
-    for line in lines.iter().take(lines.len() - 1) {
-        let mut n = 0;
-        let mut p = 0;
-
-        for c in line.chars().filter(|c| *c != '\n') {
-            if c == ' ' {
-                if n >= lens[p] {
-                    p += 1;
-                    n = 0;
-
-                    continue;
+        numbers.push(String::new());
+        for i in 0..lines.len() {
+            match lines[i][j] {
+                '+' => {
+                    answers.push(
+                        numbers
+                            .iter()
+                            .filter_map(|s| s.parse::<usize>().ok())
+                            .sum::<usize>(),
+                    );
+                    skip = true;
                 }
-            } else {
-                problems[p][n].push(c);
+                '*' => {
+                    answers.push(
+                        numbers
+                            .iter()
+                            .map(|s| s.parse::<usize>().unwrap())
+                            .reduce(|acc, e| acc * e)
+                            .unwrap(),
+                    );
+                    skip = true;
+                }
+                ' ' => continue,
+                _ => numbers.last_mut().unwrap().push(lines[i][j]),
             }
-
-            n += 1;
         }
     }
 
-    problems
-        .into_iter()
-        .enumerate()
-        .map(|(i, nums)| {
-            nums.into_iter()
-                .map(|s| s.parse::<usize>().unwrap())
-                .reduce(|acc, e| match ops[i] {
-                    '+' => acc + e,
-                    '*' => acc * e,
-                    _ => panic!("Invalid op: {}", ops[i]),
-                })
-                .unwrap()
-        })
-        .sum()
+    answers.iter().sum()
 }
 
 pub fn run(benchmark: bool) -> io::Result<()> {
